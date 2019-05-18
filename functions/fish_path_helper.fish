e24594b78fbc4a541269d48af5d2eb96e3ea3040
Chuhan Feng <simonrouse9461@gmail.com>
2019-05-17 21:07:31 -0700

    add stdin support and colon support

--- functions/fish_path_helper.fish
+++ functions/fish_path_helper.fish
@@ -1,3 +1,9 @@
+
+# TODO: 
+# add paths from file and dirs
+# modes
+# warning
+
 function fish_path_helper -d "Generate PATH configuration commands"
   argparse --name=(status function)       \
     (fish_opt --short=h --long=help)      \
@@ -6,22 +12,25 @@ function fish_path_helper -d "Generate PATH configuration commands"
     (fish_opt --short=o --long=override)  \
     (fish_opt --short=v --long=verbose)   \
     (fish_opt --short=s --long=suppress)  \
+    (fish_opt --short=f --long=files)     \
   -- $argv
 
   set -q XDG_CONFIG_HOME
     and set config_home $XDG_CONFIG_HOME
     or set config_home ~/.config
 
-  function __generate_code
-    for file in $argv[2] $argv[2].d/*
-      test -n "$argv[3]"
+  function __generate_code -V _flag_v -V _flag_s
+    for file in "$argv[2]" $argv[2].d/*
+      set -q _flag_v
         and echo (set_color green)"generate $argv[1] from $file"(set_color normal) 1>&2
-      for line in (cat $file ^ /dev/null | grep -v '^\s*#')
-        set expanded_line (eval "echo $line")
-        test -d $expanded_line
-          and echo "set -gxp $argv[1] $expanded_line;"
-          or test -n "$argv[4]"
-          or echo (set_color red)"error in file $file: $line is not a valid directory!"(set_color normal) 1>&2
+      eval "cat $file" ^ /dev/null | egrep -v '^\s*(#|$)' | while read -l line
+        set --path expanded_line (eval "echo $line")
+        for path in $expanded_line
+          test -d $path
+            and echo "set -gxp $argv[1] $path;"
+            or set -q _flag_s
+            or echo (set_color red)"error"(test $file; and echo " in file $file"; or echo)": $path is not a valid directory!"(set_color normal) 1>&2
+        end
       end
     end
   end
@@ -33,11 +42,15 @@ function fish_path_helper -d "Generate PATH configuration commands"
     echo "set -gx MANPATH;"
   end
   set -q _flag_g
-    and __generate_code PATH /etc/paths "$_flag_v" "$_flag_s"
-    and __generate_code MANPATH /etc/manpaths "$_flag_v" "$_flag_s"
+    and __generate_code PATH /etc/paths
+    and __generate_code MANPATH /etc/manpaths
   set -q _flag_l
-    and __generate_code PATH {$config_home}/paths "$_flag_v" "$_flag_s"
-    and __generate_code MANPATH {$config_home}/manpaths "$_flag_v" "$_flag_s"
-  
+    and __generate_code PATH {$config_home}/paths
+    and __generate_code MANPATH {$config_home}/manpaths
+  not set -q _flag_g 
+    and not set -q _flag_l
+    and not set -q _flag_f
+    and __generate_code PATH
+
   functions -e __generate_code
 end
