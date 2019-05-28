52d4cb622c498962744f47f6bfb0d7917f80628f
Chuhan Feng <simonrouse9461@gmail.com>
2019-05-28 03:58:04 -0700

    change path config dir to $XDG_CONFIG_HOME/env/

--- init.fish
+++ init.fish
@@ -10,7 +10,7 @@ if status is-login
     and set config_home $XDG_CONFIG_HOME
     or set config_home ~/.config
 
-  path load -p {$config_home}/paths {$config_home}/paths.d
-  path load -m {$config_home}/manpaths {$config_home}/manpaths.d
+  path load -p {$config_home}/env/paths {$config_home}/env/paths.d
+  path load -m {$config_home}/env/manpaths {$config_home}/env/manpaths.d
 
 end
